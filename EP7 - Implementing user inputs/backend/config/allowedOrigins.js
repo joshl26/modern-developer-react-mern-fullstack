7a7d0873aa056091ea51b4b542235b17b3347edb
Joshua Lehman <joshlehman.dev@gmail.com>
2023-02-05 06:43:22 -0500

    commit

---
+++ EP7 - Implementing user inputs/backend/config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://www.dandrepairshop.com",
+  "https://dandrepairshop.com",
+  "https://www.google.ca",
+];
+
+module.exports = allowedOrigins;
