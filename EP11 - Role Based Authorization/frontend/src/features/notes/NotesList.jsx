6ca30b7c42920001f9a9e8d9f6ed728f7867a45c
Joshua Lehman <joshlehman.dev@gmail.com>
2023-02-07 11:00:59 -0500

    commit

--- EP11 - Role Based Authorization/frontend/src/features/notes/NotesList.jsx
+++ EP11 - Role Based Authorization/frontend/src/features/notes/NotesList.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { useGetNotesQuery } from "./notesApiSlice";
 import Note from "./Note";
+import useAuth from "../../hooks/useAuth";
 
 const NotesList = () => {
+  const { username, isManager, isAdmin } = useAuth();
+
   const {
     data: notes,
     isLoading,
     isSuccess,
     isError,
     error,
-  } = useGetNotesQuery('noteslist', {
+  } = useGetNotesQuery("noteslist", {
     pollingInterval: 15000,
     refetchOnFocus: true,
     refetchOnMountOrArgChange: true,
@@ -24,12 +27,21 @@ const NotesList = () => {
   }
 
   if (isSuccess) {
-    const { ids } = notes;
+    const { ids, entities } = notes;
 
-    const tableContent = ids?.length
-      ? ids.map((noteId) => <Note key={noteId} noteId={noteId} />)
-      : null;
+    let filteredIds;
 
+    if (isManager || isAdmin) {
+      filteredIds = [...ids];
+    } else {
+      filteredIds = ids.filter(
+        (noteId) => entities[noteId].username === username
+      );
+    }
+
+    const tableContent =
+      ids?.length &&
+      filteredIds.map((noteId) => <Note key={noteId} noteId={noteId} />);
     content = (
       <table className="table table--notes">
         <thead className="table__head">
