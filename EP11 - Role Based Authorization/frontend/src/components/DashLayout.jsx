565683c499ae387577f22e988baaeea5e9755528
Joshua Lehman <joshlehman.dev@gmail.com>
2023-02-07 01:07:40 -0500

    commit

---
+++ EP11 - Role Based Authorization/frontend/src/components/DashLayout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import DashHeader from "./DashHeader";
+import DashFooter from "./DashFooter";
+import { Outlet } from "react-router-dom";
+
+const DashLayout = () => {
+  return (
+    <>
+      <DashHeader />
+      <div className="dash-container">
+        <Outlet />
+      </div>
+      <DashFooter />
+    </>
+  );
+};
+
+export default DashLayout;
---
+++ EP11 - Role Based Authorization/readme.md
@@ -0,0 +1,5 @@
+The Modern Developer Youtube Channel
+
+React MERN Full Stack App
+
+EP10 - Persist Login Authentication on Refresh
