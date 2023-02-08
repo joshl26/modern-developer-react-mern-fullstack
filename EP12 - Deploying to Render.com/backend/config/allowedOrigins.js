10641aadeba5643887987281f255644105a5aef3
Joshua Lehman <joshlehman.dev@gmail.com>
2023-02-08 16:56:03 -0500

    commit

--- EP12 - Deploying to Render.com/backend/config/allowedOrigins.js
+++ EP12 - Deploying to Render.com/backend/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = ["https://mern-test-frontend-vzc1.onrender.com"];
 
 module.exports = allowedOrigins;
--- EP12 - Deploying to Render.com/frontend/src/app/api/apiSlice.js
+++ EP12 - Deploying to Render.com/frontend/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://mynotes-api.onrender.com",
+  baseUrl: "https://mern-test-backend.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
